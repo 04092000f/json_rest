fe55be1981cbf62896a7a8a20bfc5fcaf625ca77
04092000f <93308703+04092000f@users.noreply.github.com>
2022-05-31 16:06:41 +0530

    Add files via upload

--- json_rest/api.py
+++ json_rest/api.py
@@ -1,1 +1,7 @@
+from rest_framework import routers
+from mozio_rest.views import ProviderViewset, GeojsonViewset
 
+
+router = routers.DefaultRouter()
+router.register(r'providers', ProviderViewset)
+router.register(r'geojsons', GeojsonViewset)
